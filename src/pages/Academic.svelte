7c55b44720c92732271bf456c34a08ca316e5296
Bugi Insan Kamil <bugi.detkp@gmail.com>
2022-09-17 07:09:47 +0700

    refactoring navigations

--- src/pages/Academic.svelte
+++ src/pages/Academic.svelte
@@ -1,1 +1,9 @@
-Academic Page
+<script>
+  import Sidebar from "../components/navigations/Sidebar.svelte";
+
+  const sidebarTitle = "Academic Menu";
+  const sidebarMenu = [];
+
+</script>
+
+<Sidebar {sidebarTitle} {sidebarMenu} />
--- src/pages/Finance.svelte
+++ src/pages/Finance.svelte
@@ -1,1 +1,9 @@
-Finance Page
+<script>
+  import Sidebar from "../components/navigations/Sidebar.svelte";
+
+  const sidebarTitle = "Finance Menu";
+  const sidebarMenu = [];
+
+</script>
+
+<Sidebar {sidebarTitle} {sidebarMenu} />
